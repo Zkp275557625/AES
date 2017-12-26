ac5fff401546445599b4d65b2c226cd8daaa4cbc
Zkp275557625 <275557625@qq.com>
2017-12-26 17:17:14 +0800

    跨平台AES加密解密

--- src/testAES.java
+++ src/testAES.java
@@ -2,20 +2,21 @@
  * 作者：zhoukp
  * 时间：2017/12/25 16:15
  * 邮箱：zhoukaiping@szy.cn
- * 作用：测试AES加密解密
+ * 作用：AES加密解密 测试类
  */
 public class testAES {
     public static void main(String[] args) throws Exception {
-        String content = "zhoukp";
+        String content = "应老师真帅";
         System.out.println("加密前：" + content);
 
-        String key = "hqucstzkp";
-        System.out.println("加密密钥和解密密钥：" + key);
+        String key = MD5.encode("hqucstzkp");
+        System.out.println("秘钥：" + key);
 
-        String encrypt = AES.aesEncrypt(content, key);
+        String encrypt = AesUtil.encrypt(content, key);
         System.out.println("加密后：" + encrypt);
 
-        String decrypt = AES.aesDecrypt(encrypt, key);
+//        String decrypt = AesUtil.decrypt(encrypt, key);
+        String decrypt = AesUtil.decrypt("EbJumUw23By8ssBFpeYcVA==", key);
         System.out.println("解密后：" + decrypt);
     }
 }
